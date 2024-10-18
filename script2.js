9728a7176ac4885b809538dd043531cb41ce0d09
Daisy306dev <developinglhs@naver.com>
2024-10-19 06:52:29 +0900

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,32 @@
+function nnnnN() {
+    document.querySelector("p").innerHTML = `this is a dog`;
+}
+document.getElementById("name").onclick = nnnnN;
+
+function nnnnN2() {
+    document.querySelector("p").innerHTML = `this is shibainu`;
+}
+document.getElementById("breed").onclick = nnnnN2;
+
+function nnnnN3() {
+    document.querySelector("p").innerHTML = `this is confident`;
+}
+document.getElementById("nature").onclick = nnnnN3;
+
+
+
+
+function nnnnN4() {
+    document.getElementById("par2").innerHTML = `this is a cat`;
+}
+document.getElementById("name2").onclick = nnnnN4;
+
+function nnnnN5() {
+    document.getElementById("par2").innerHTML = `this is Mackerel tabby`;
+}
+document.getElementById("breed2").onclick = nnnnN5;
+
+function nnnnN6() {
+    document.getElementById("par2").innerHTML = `this is moody`;
+}
+document.getElementById("nature2").onclick = nnnnN6;
